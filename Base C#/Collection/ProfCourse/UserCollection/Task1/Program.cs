730b672dbcab2e6647be98323e5c730682152d19
GSE995 <gorynovse@gmail.com>
2017-09-29 18:22:22 +0300

    UserCollection

---
+++ Base C#/Collection/ProfCourse/UserCollection/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           MonthCollection first = new MonthCollection();
+
+            foreach (string month in first.GetMothForCountDay(31))
+            {
+                Console.WriteLine(month);
+            }
+        }
+    }
+}
