822f04718634e15bb0e11b03c5b5ebc767c7d414
GSE995 <gorynovse@gmail.com>
2017-10-07 13:18:25 +0300

    WeatherAppWindowsForms

---
+++ Applications/WeatheApp/WeatheApp/OpenWeather/Main.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatheApp.OpenWeather
+{
+    class Main
+    {
+        double _tempmin;
+        public double temp_min
+        {
+            get { return _tempmin; }
+            set { _tempmin = value - 273.5; }
+        }
+
+        double _temptmax;
+        public double temp_max
+        {
+            get { return _temptmax; }
+            set { _temptmax = value - 278; }
+        }
+
+        double _presure;
+        public double presure
+        {
+            get
+            {
+                return _presure;
+            }
+            set
+            {
+                _presure = value / 1.3332239;
+            }
+        }
+        public double humidity;
+
+        public double Temp
+        {
+            get { return (_tempmin + _temptmax) / 2; }
+        }
+    }
+}
---
+++ Applications/WeatheApp/WeatheApp/OpenWeather/Sys.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatheApp.OpenWeather
+{
+    class Sys
+    {
+        public int type;
+        public int id;
+        public double message;
+        public string country;
+        public double sunrise;
+        public double sunset;
+
+    }
+}
