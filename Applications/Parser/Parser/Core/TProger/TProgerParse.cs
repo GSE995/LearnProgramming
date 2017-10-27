5afbdc152861e02664f9d18f4f5c8e3a1ff8af86
GSE995 <gorynovse@gmail.com>
2017-10-27 15:27:18 +0300

    ParserTitle

---
+++ Applications/Parser/Parser/Core/TProger/TProgerParse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp.Dom.Html;
+
+namespace Parser.Core.TProger
+{
+    class TProgerParse : IParser<string[]>
+    {
+        public string[] Parse(IHtmlDocument document)
+        {
+            var list = new List<string>();
+            var items = document.QuerySelectorAll("span").Where(item => item.ClassName != null && item.ClassName.Contains("entry-title-heading"));
+            foreach (var item in items)
+            {
+                list.Add(item.TextContent);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
