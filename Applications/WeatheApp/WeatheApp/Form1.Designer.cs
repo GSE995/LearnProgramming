c759adf5d2f4320ca35d5aa77c7d9833311c6a74
GSE995 <gorynovse@gmail.com>
2017-10-27 15:19:49 +0300

    WeatheApp

--- Applications/WeatheApp/WeatheApp/Form1.Designer.cs
+++ Applications/WeatheApp/WeatheApp/Form1.Designer.cs
@@ -30,15 +30,19 @@
         {
             this.diaplayWeatherData = new System.Windows.Forms.RichTextBox();
             this.mainGroupBox = new System.Windows.Forms.GroupBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.inputCity = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBoxWind = new System.Windows.Forms.GroupBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.city = new System.Windows.Forms.Label();
             this.mainGroupBox.SuspendLayout();
             this.groupBoxWind.SuspendLayout();
             this.SuspendLayout();
@@ -47,13 +51,16 @@
             // 
             this.diaplayWeatherData.Location = new System.Drawing.Point(12, 12);
             this.diaplayWeatherData.Name = "diaplayWeatherData";
-            this.diaplayWeatherData.Size = new System.Drawing.Size(403, 73);
+            this.diaplayWeatherData.Size = new System.Drawing.Size(403, 48);
             this.diaplayWeatherData.TabIndex = 0;
             this.diaplayWeatherData.Text = "";
             this.diaplayWeatherData.TextChanged += new System.EventHandler(this.diaplayWeatherData_TextChanged);
             // 
             // mainGroupBox
             // 
+            this.mainGroupBox.Controls.Add(this.label8);
+            this.mainGroupBox.Controls.Add(this.inputCity);
+            this.mainGroupBox.Controls.Add(this.button1);
             this.mainGroupBox.Controls.Add(this.groupBoxWind);
             this.mainGroupBox.Controls.Add(this.label5);
             this.mainGroupBox.Controls.Add(this.label4);
@@ -61,65 +68,29 @@
             this.mainGroupBox.Controls.Add(this.label2);
             this.mainGroupBox.Controls.Add(this.label1);
             this.mainGroupBox.Controls.Add(this.panel1);
-            this.mainGroupBox.Location = new System.Drawing.Point(13, 91);
+            this.mainGroupBox.Location = new System.Drawing.Point(13, 108);
             this.mainGroupBox.Name = "mainGroupBox";
             this.mainGroupBox.Size = new System.Drawing.Size(402, 263);
             this.mainGroupBox.TabIndex = 1;
             this.mainGroupBox.TabStop = false;
             this.mainGroupBox.Text = "Погода";
             // 
-            // panel1
+            // inputCity
             // 
-            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.panel1.Location = new System.Drawing.Point(7, 20);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(104, 103);
-            this.panel1.TabIndex = 0;
+            this.inputCity.Location = new System.Drawing.Point(277, 158);
+            this.inputCity.Name = "inputCity";
+            this.inputCity.Size = new System.Drawing.Size(100, 20);
+            this.inputCity.TabIndex = 8;
             // 
-            // label1
+            // button1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(118, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Главное";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(118, 62);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Описание";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(118, 110);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(118, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Средняя температура";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(257, 20);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(63, 13);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Влажность";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(257, 62);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(58, 13);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Давление";
+            this.button1.Location = new System.Drawing.Point(277, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // groupBoxWind
             // 
@@ -132,15 +103,6 @@
             this.groupBoxWind.TabStop = false;
             this.groupBoxWind.Text = "Ветер";
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 26);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(55, 13);
-            this.label6.TabIndex = 4;
-            this.label6.Text = "Скорость";
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
@@ -150,11 +112,92 @@
             this.label7.TabIndex = 5;
             this.label7.Text = "Ветер";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Скорость";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(257, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Давление";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(257, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Влажность";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Средняя температура";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Описание";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Главное";
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Location = new System.Drawing.Point(7, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(104, 103);
+            this.panel1.TabIndex = 0;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(277, 139);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(81, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Введите город";
+            // 
+            // city
+            // 
+            this.city.AutoSize = true;
+            this.city.Location = new System.Drawing.Point(180, 92);
+            this.city.Name = "city";
+            this.city.Size = new System.Drawing.Size(46, 13);
+            this.city.TabIndex = 2;
+            this.city.Text = "Ivanovo";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(427, 366);
+            this.ClientSize = new System.Drawing.Size(427, 383);
+            this.Controls.Add(this.city);
             this.Controls.Add(this.mainGroupBox);
             this.Controls.Add(this.diaplayWeatherData);
             this.Name = "Form1";
@@ -165,6 +208,7 @@
             this.groupBoxWind.ResumeLayout(false);
             this.groupBoxWind.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -181,6 +225,10 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox inputCity;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label city;
     }
 }
 
--- Applications/WeatheApp/WeatheApp/Form1.cs
+++ Applications/WeatheApp/WeatheApp/Form1.cs
@@ -22,11 +22,11 @@ namespace WeatheApp
             InitializeComponent();
         }
 
-       
-
-        private async void Form1_Load(object sender, EventArgs e)
+        private async void Loader(string arg)
         {
-            WebRequest request = WebRequest.Create("http://api.openweathermap.org/data/2.5/weather?q=Ivanovo&APPID=1a2762f5f6be54e3889c29287ed18a46");
+            city.Text = arg;
+
+            WebRequest request = WebRequest.Create($"http://api.openweathermap.org/data/2.5/weather?q={arg}&APPID=1a2762f5f6be54e3889c29287ed18a46");
 
             request.Method = "POST";
 
@@ -60,9 +60,21 @@ namespace WeatheApp
             label2.Text = ow.weather[0].description;
         }
 
+       
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            Loader("Ivanovo");
+        }
+
         private async void diaplayWeatherData_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Loader(inputCity.Text);
         }
     }
 }
