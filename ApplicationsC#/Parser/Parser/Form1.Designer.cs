942b152180e43f01e008ab6ba7b14ac329647161
GSE995 <gorynovse@gmail.com>
2017-12-13 01:10:09 +0300

    AppsC#

---
+++ ApplicationsC#/Parser/Parser/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Parser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listTitles = new System.Windows.Forms.ListBox();
+            this.numericStart = new System.Windows.Forms.NumericUpDown();
+            this.numericEnd = new System.Windows.Forms.NumericUpDown();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonEnd = new System.Windows.Forms.Button();
+            this.inputSection = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericStart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericEnd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listTitles
+            // 
+            this.listTitles.FormattingEnabled = true;
+            this.listTitles.Location = new System.Drawing.Point(13, 33);
+            this.listTitles.Name = "listTitles";
+            this.listTitles.Size = new System.Drawing.Size(512, 355);
+            this.listTitles.TabIndex = 0;
+            this.listTitles.SelectedIndexChanged += new System.EventHandler(this.listTitles_SelectedIndexChanged);
+            // 
+            // numericStart
+            // 
+            this.numericStart.Location = new System.Drawing.Point(531, 126);
+            this.numericStart.Name = "numericStart";
+            this.numericStart.Size = new System.Drawing.Size(120, 20);
+            this.numericStart.TabIndex = 1;
+            // 
+            // numericEnd
+            // 
+            this.numericEnd.Location = new System.Drawing.Point(531, 165);
+            this.numericEnd.Name = "numericEnd";
+            this.numericEnd.Size = new System.Drawing.Size(120, 20);
+            this.numericEnd.TabIndex = 2;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(551, 191);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonEnd
+            // 
+            this.buttonEnd.Location = new System.Drawing.Point(551, 220);
+            this.buttonEnd.Name = "buttonEnd";
+            this.buttonEnd.Size = new System.Drawing.Size(75, 23);
+            this.buttonEnd.TabIndex = 4;
+            this.buttonEnd.Text = "Abort";
+            this.buttonEnd.UseVisualStyleBackColor = true;
+            this.buttonEnd.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // inputSection
+            // 
+            this.inputSection.Location = new System.Drawing.Point(537, 61);
+            this.inputSection.Name = "inputSection";
+            this.inputSection.Size = new System.Drawing.Size(100, 20);
+            this.inputSection.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(531, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Cо страницы:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(531, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "По";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(534, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Введите раздел";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 404);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputSection);
+            this.Controls.Add(this.buttonEnd);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.numericEnd);
+            this.Controls.Add(this.numericStart);
+            this.Controls.Add(this.listTitles);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericStart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericEnd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listTitles;
+        private System.Windows.Forms.NumericUpDown numericStart;
+        private System.Windows.Forms.NumericUpDown numericEnd;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonEnd;
+        private System.Windows.Forms.TextBox inputSection;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
