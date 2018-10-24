58334346b176280ef78550a2aa71d27a5ffb2290
sgoryunov <sgoryunov@loginet.ru>
2018-10-24 17:07:04 +0300

    refactored

--- Applications JavScript/FindMovie/src/js/components/movi-list/list.js
+++ Applications JavScript/FindMovie/src/js/components/movi-list/list.js
@@ -1,89 +1,73 @@
-import MovieCard from '../movie/movie-card';
-import config from '../../config';
+import MovieCard from '../movie/movie-card'
 
-export default class MovieList{
+export default class MovieList {
 
-    constructor(data){
+    constructor(el){
+        this.el = el;
+    }
+
+    setMovies(data){
         this.data = data;
-        this.moviecard = new MovieCard();
+        this.renderMovies(data)
     }
 
-    drawToDom(selector){
-        this.selector = selector;
-        this.clearList(selector);
-        selector.appendChild(this.fragment);
-    }
-    
     renderMovies(data){
-        this.fragment = document.createDocumentFragment();
+        this.clearList();
 
-        data.forEach(data => {
-            const arcticle = document.createElement('div');
-            arcticle.classList.add('col-6');
-            arcticle.classList.add('col-md-3');
-            arcticle.classList.add('movies');
-            arcticle.innerHTML = this.moviecard.getHTML(data);
-            this.fragment.appendChild(arcticle);
+        data.forEach(movie => {
+            let card = new MovieCard(movie);
+            card.renderTo(this.el);
         });
     }
-    clearList(selector){
-        selector.innerHTML = '';
+    
+    clearList(){
+        this.el.innerHTML = '';
     }
 
     sort(filter){
-        const data = [...this.data.results];
-        if(filter === 'reiting-max'){
-            this.sortByMaxReiting(data);
-        }
-        if(filter === 'reiting-min'){
-            this.sortByMinReiting(data);
-        }
-        if(filter === 'new'){
-            this.sortNew(data);
-        }
-        if(filter === 'old'){
-            this.sortOld(data);
+        const data = [...this.data];
+
+        switch(filter){
+            case 'reiting-max': 
+                this.sortByMaxReiting(data);
+                break;
+            case 'reiting-min':
+                this.sortByMinReiting(data);
+                break;
+            case 'new':
+                this.sortNew(data);
+                break;
+            case 'old':
+                this.sortOld(data);
+                break;
         }
     }
 
     sortByMaxReiting(data){
-        data.sort((a,b)=>{
-            if(a.popularity < b.popularity)return 1;
-            if(a.popularity > b.popularity) return -1;
-        })
+        data.sort((a,b)=> (a.popularity < b.popularity) ? 1 : -1);
         this.renderMovies(data);
-        this.drawToDom(document.querySelector('.movies'));
     }
+
     sortByMinReiting(data){
-        data.sort((a,b)=>{
-            if(a.popularity > b.popularity)return 1;
-            if(a.popularity < b.popularity)return -1;
-        })
+        data.sort((a, b) => (a.popularity > b.popularity) ? 1 : -1);
         this.renderMovies(data);
-        this.drawToDom(document.querySelector('.movies'));
     }
+
     sortNew(data){
         data.sort((a,b)=>{
             let aDate = new Date(a.first_air_date || a.release_date);
             let bDate = new Date(b.first_air_date || b.release_date);
-            if(aDate < bDate) return 1;
-            if(aDate > bDate) return -1; 
+            return (aDate < bDate) ? 1 : -1;
         })
         this.renderMovies(data);
-        this.drawToDom(document.querySelector('.movies'));      
     }
+
     sortOld(data){
         data.sort((a,b)=>{
             let aDate = new Date(a.first_air_date || a.release_date);
             let bDate = new Date(b.first_air_date || b.release_date);
-            if(aDate > bDate) return 1;
-            if(aDate < bDate) return -1; 
+            return (aDate > bDate) ? 1 : -1;
         })
         this.renderMovies(data);
-        this.drawToDom(document.querySelector('.movies'));   
-    }
-
-    hide(params) {
-        this.selector.style.display = 'none';  
     }
 }
