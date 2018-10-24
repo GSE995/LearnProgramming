acd281f0672ff13a449c265b43be21a850b8fe37
sgoryunov <sgoryunov@loginet.ru>
2018-10-24 17:53:33 +0300

    upd movie-card: add method for insert card in html element

--- Applications JavScript/FindMovie/src/js/components/movie/movie-card.js
+++ Applications JavScript/FindMovie/src/js/components/movie/movie-card.js
@@ -1,15 +1,33 @@
 import config from '../../config';
 
-export default class MovieCard{
+export default class MovieCard {
+
+    constructor(data){
+        this.data = data;
+    }
+
+    renderTo(fragment){
+        let wrapper = this.createCardWrapper(this.data);
+        fragment.appendChild(wrapper);
+    }
+
+    createCardWrapper(movie){
+        let arcticle = document.createElement('div');
+        arcticle.classList.add('col-6');
+        arcticle.classList.add('col-md-3');
+        arcticle.classList.add('movies');
+        arcticle.innerHTML = this.getHTML(movie);
+        return arcticle;
+    }
 
     getHTML(data){
         this.data = this.mapData(data);
         const html = `
         <article class="card">
             <a href="${this.data.id}" class="movie-link" >
-            <header class="title-header">
-                <h3>${this.data.title} ${this.data.year}</h3>
-            </header>
+                <header class="title-header">
+                    <h3>${this.data.title} ${this.data.year}</h3>
+                </header>
             </a>
             <div class="card-block">
                 <a href="${this.data.id}" class="movie-link" >
@@ -44,16 +62,11 @@ export default class MovieCard{
 
     setPic(data){
         const src = data.poster_path || data.backdrop_path;
-        if(!src){
-            return config.noImagSrc;
-        }
-        return config.imgUrl + src;
+        return (!src) ? config.noImagSrc : `${config.imgUrl}${src}`;
     }
 
     validOverview(text){
-        if(text == null){
-            return 'unknown'
-        }
+        if(!text) return 'unknown';
         let mas = text.split('');
         let str = mas.slice(0, 120).join('') + '...';
         return str;
